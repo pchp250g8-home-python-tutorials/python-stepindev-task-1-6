7f1327d1bfd6cf22066dd6a47d532e6aae650994
Alexander <pchp250g8@tutamail.com>
2026-09-15 19:36:22 +0300

    Fixed Logical Errors

--- Task-1-6-en.py
+++ Task-1-6-en.py
@@ -1,11 +1,14 @@
 # --coding:utf-8--
+
+import math
+
 n = int(input("Enter fence length (in meters): "))  # Fence length
 # --Paint consumption rate--
 k = int(input("How many meters does a can of paint last for? "))
 # Cost of one can of paint
 c = int(input("Price of one can of paint (in rubles):"))
 # Number of cans of paint for the entire length of the fence
-b = n // k + n % k
+b = math.ceil(n / k)
 p = b * c  # Cost of all cans of paint for the entire fence
 # -- Display information on the screen --
 print(f"{b} cans of paint are needed for {n} meters of fence.")
--- Task-1-6-ru.cmd
+++ Task-1-6-ru.cmd
@@ -4,5 +4,5 @@
 call "%ANACONDA_ROOT%\Scripts\activate.bat" %ANACONDA_ROOT%
 @set PATH=%VSCODE_ROOT%;%PATH%
 @cls
-python %~dp0\Task-1-6.py
+python %~dp0\Task-1-6-ru.py
 pause >nul
